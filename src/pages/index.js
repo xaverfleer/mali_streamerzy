bce5aeb38ab5d9444f2f8c1b311c57aa1ab5e376
Dominik Kurbiel <dominik.kurbiel@jazzy.pro>
2020-09-28 23:01:30 +0200

    DEV-8: Background images changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import React from "react"
 
 import Layout from "../components/layout"
 // import Image from "../components/image"
-import SEO from "../components/seo"
+import SEO from "../components/SEO"
 
 function IndexPage() {
     return <Layout>
@@ -20,6 +20,6 @@ function IndexPage() {
         {/*<Link to="/page-2/">Go to page 2</Link> <br/>*/}
         {/*<Link to="/using-typescript/">Go to "Using TypeScript"</Link>*/}
     </Layout>
-};
+}
 
 export default IndexPage
